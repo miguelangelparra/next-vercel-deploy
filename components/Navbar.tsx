496df323d841d7a4d7a05415150b67ca33b488fd
miguelangelparra <miguelangel.parradavila@gmail.com>
2022-04-12 18:51:35 -0300

    vercel-deploy

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { ActiveLink } from "./ActiveLink";
+import styles from "./Navbar.module.css";
+
+const menuItems = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Contact",
+    href: "/contact",
+  },
+  {
+    text: "Pricing",
+    href: "/pricing",
+  },
+];
+
+export const Navbar:FC = () => {
+  return (
+    <nav className={styles["menu-container"]}>
+      {menuItems.map(({ text, href }) => (
+        <ActiveLink key={href} text={text} href={href} />
+      ))}
+    </nav>
+  );
+};
---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+
+
+export const DarkLayout:FC = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "rgba(0,0,0,0.3",
+        borderRadius: "5px",
+        padding: "10px",
+      }}
+    >
+      <h3>DarkLayout</h3>
+      <div>{children}</div>
+    </div>
+  );
+};
